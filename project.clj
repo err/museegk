f24375d126ccce243afea68e9908785976f8e2c5
eric caspary <caspary.eric@gmail.com>
2010-11-02 15:20:15 -0400

    first real commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject museegk "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [org.clojars.technomancy/rosado.processing "1.1.0"]
+		 [overtone "0.1.3-SNAPSHOT"]
+		 [overtone/clj-native "0.6.2-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
